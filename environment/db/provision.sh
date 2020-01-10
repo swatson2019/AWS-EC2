d3c05f71000e4e349064f3521c19858a46da0d9c
Sam Watson <SWatson@spartaglobal.com>
2020-01-10 12:16:40 +0000

    test

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -1,6 +1,5 @@
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
